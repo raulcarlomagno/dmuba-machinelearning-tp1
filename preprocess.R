a24d8630196db8dc5b80d577b4ac79f9fb24cef6
Raul Carlomagno <rcarlomagno@gmail.com>
2018-04-25 14:56:57 -0300

    better final csv

--- preprocess.R
+++ preprocess.R
@@ -17,19 +17,43 @@ Sys.setlocale("LC_TIME", "C") #para el nombre del mes en ingles
 df$month_launched <- as.factor(format(df$launched, format="%B"))
 df <- df[, -which(colnames(df) %in% c("launched","deadline"))]
 df <- df[df$state %in% c("successful","failed"),]
-df$funded <- as.logical(ifelse(df$state == "successful", 1, 0))
+df$funded <- as.factor(ifelse(df$state == "successful", "yes", "no"))
 df <- df[, -which(colnames(df) == "state")]
 
 df <- droplevels(df) #refresh factors
 
-write.csv(df, "ks-projects-processed.csv")
+
+currencies <- c("AUD", "CAD", "CHF", "DKK", "EUR", "GBP", "HKD", "JPY", "MXN", "NOK", "NZD", "SEK", "SGD", "USD")
+usd_values <- c(0.76038, 0.77924, 1.02181, 0.16403, 1.22191, 1.39564, 0.12746, 0.00919, 0.05298, 0.12665, 0.71193, 0.11749, 0.75569, 1)
+dfCurrencies <- data.frame(currency = currencies, usd_value = usd_values)
+df$usd_goal <- round((df$goal * dfCurrencies[match(df$currency, dfCurrencies$currency), 2]), 2)
+df$goal <- NULL #remove column
+df$usd_goal_real  <- NULL #remove column
+df <- df[df$usd_goal >= 1000 & df$usd_goal <= 100000, ] #nos quedamos con los proyectos entre 1000 y 100000 de objetivo a recaudar
+df <- df[, c(setdiff(names(df), c("funded")), c("funded"))] #columna funded al final
 
 
-summary(df$currency)
 
-hist(df$launched,breaks = "month")
+write.csv(df, "ks-projects-processed.csv", row.names=FALSE)
+
+
+goal <- df$usd_goal
+goal.iqr <- IQR(goal)
+goal.q3 = quantile(goal, 0.75)
+goal.q1 = quantile(goal, 0.25)
+df[df$usd_goal > goal.q3 + goal.iqr * 3, ]
+df[df$usd_goal < goal.q1 - goal.iqr * 1.5, ]
+
+
+View(df[df$currency != "USD",])
+
+str(dfCurrencies)
+str(df)
+head(df)
+summary(df)
+hist(df$usd_goal)
 hist(df$days)
-plot(df$main_category)
+plot(df$month_launched)
 
-boxplot(df$usd_goal_real)
-
+pie(table(df$main_category));
+boxplot(usd_goal ~ country, data = df)
