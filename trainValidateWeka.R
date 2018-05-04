ef8b7e3593502aa423a05e6c2d0aa263c7e934ac
Raul Carlomagno <rcarlomagno@gmail.com>
2018-05-04 18:16:33 -0300

    calling weka

--- trainValidateWeka.R
+++ trainValidateWeka.R
@@ -3,19 +3,21 @@ library("RWeka")
 
 trainData <- read.csv("ks-projects-processed.csv")
 
-WOW("J48")
+#WOW("J48")
 
 J48Options <- Weka_control(R = TRUE, M = 5)
 m <- J48(funded ~ ., data = trainData, control = J48Options)
-summary(m)
+#summary(m) #calls evaluate_Weka_classifier()
+table(trainData$funded, predict(m)) 
+#pag 20
 
-e <- evaluate_Weka_classifier(m,
-                              cost = matrix(c(0,2,1,0), ncol = 2),
-                              numFolds = 10, complexity = TRUE,
-                              seed = 123, class = TRUE)
 
-e
-summary(e)
-e$details
+crossValEval <- evaluate_Weka_classifier(m,
+                              #cost = matrix(c(0,1,1,0), ncol = 2),
+                              numFolds = 10, class = TRUE)
+
+crossValEval
+summary(crossValEval)
+e$details[["pctCorrect"]]
 
 
