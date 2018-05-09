4a9da4e373d675ae4f44c0e27783363536b61efe
Raul Carlomagno <rcarlomagno@gmail.com>
2018-05-09 11:02:10 -0300

    better

--- trainValidateWeka.R
+++ trainValidateWeka.R
@@ -17,12 +17,11 @@ doTraining <- function(j48ParamName, nombreParametro, valoresParametro){
   dfSize <- data.frame(valorParam = double(), leavesNumber = integer(), treeSize = integer())
 
   for(valorParametro in valoresParametro) {
-    print(paste("ENTRENANDO ARBOL CON", nombreParametro, valorParametro))
+    print(sprintf("entrenando arbol con %s (%s) = %.2f", nombreParametro, j48ParamName, valorParametro))
   
-    j48Params[1] = valorParametro
+    j48Params[1] <- valorParametro
     J48Options <- do.call(Weka_control, as.list(j48Params))
     
-    #J48Options <- Weka_control(C = valorParametro)
     model <- J48(funded ~ ., data = trainData, control = J48Options)
     partyModel <- as.party(model)
     dfSize[nrow(dfSize) + 1, ] <- c(valorParametro, width(partyModel), length(partyModel))
@@ -30,8 +29,8 @@ doTraining <- function(j48ParamName, nombreParametro, valoresParametro){
     crossValEval <- evaluate_Weka_classifier(model, numFolds = 10, class = TRUE)
     testEval <- evaluate_Weka_classifier(model, newdata = testData, class = TRUE)
     
-    print(sprintf("TRAINING - PORCENTAJE CORRECTOS - %.2f", crossValEval$details[["pctCorrect"]]))
-    print(sprintf("TESTING - PORCENTAJE CORRECTOS - %.2f", testEval$details[["pctCorrect"]]))
+    print(sprintf("---accuracy training con %s (%s=%.2f) : %.2f%%", nombreParametro, j48ParamName, valorParametro, crossValEval$details[["pctCorrect"]]))
+    print(sprintf("---accuracy testing con %s (%s=%.2f): %.2f%%", nombreParametro, j48ParamName, valorParametro, testEval$details[["pctCorrect"]]))
     
     dfPerformance[nrow(dfPerformance) + 1, ] <- c(valorParametro, round(crossValEval$details[["pctCorrect"]], 2), "training")
     dfPerformance[nrow(dfPerformance) + 1, ] <- c(valorParametro, round(testEval$details[["pctCorrect"]], 2), "testing")
