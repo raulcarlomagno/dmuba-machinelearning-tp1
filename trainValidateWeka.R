f22fd2e214d14ab6bad3542a579f9ad58fd51f4d
Raul Carlomagno <rcarlomagno@gmail.com>
2018-05-13 22:13:32 -0300

    falta punto 6

--- trainValidateWeka.R
+++ trainValidateWeka.R
@@ -7,8 +7,6 @@ library("rlist")
 trainDf <- read.csv("ks-projects-processed-train.csv")
 testDf <- read.csv("ks-projects-processed-test.csv")
 
-#WOW("J48")
-
 savePlot <- function(plot, titulo, fileName){
   plot <- plot + ggtitle(titulo)
   print(sprintf("saving plot to %s", fileName))
@@ -65,11 +63,11 @@ doTraining <- function(trainData, testData, j48ParamName, nombreParametro, valor
   return(resultado)
 }
 
-trainFaltantes <- function(estrategiaModa = TRUE){
+trainFaltantes <- function(colFaltante, estrategiaModa = TRUE){
   dfPerformanceFaltantes <- data.frame(valorParam = double(), accuracyTraining = double(), accuracyTesting = double(), porcentajeFaltantes = integer())
   dfSizeFaltantes <- data.frame(valorParam = double(), leaves = integer(), nodes = integer(), porcentajeFaltantes = integer())
   
-  colName <- "month_launched" #nombre de columna para simular faltantes
+  colName <- colFaltante #nombre de columna para simular faltantes
   colIndex <- match(colName, colnames(trainDf))
   if(estrategiaModa){
     colModa <- names(tail(sort(table(trainDf[, colIndex])), 1))
@@ -138,6 +136,63 @@ trainFaltantes <- function(estrategiaModa = TRUE){
 }
 
 
+toleranciaRuido <- function(colClase, valorClase){
+  dfPerformanceRuido <- data.frame(valorParam = double(), accuracyTraining = double(), accuracyTesting = double(), porcentajeRuido = integer())
+  dfSizeRuido <- data.frame(valorParam = double(), leaves = integer(), nodes = integer(), porcentajeRuido = integer())
+  
+  colIndex <- match(colClase, colnames(trainDf))
+  colModa <- names(tail(sort(table(trainDf[, colIndex])), 1))
+  
+  ruidosPorcent <- seq(0, 0.35, 0.01)
+  for(ruidoPorcent in ruidosPorcent){
+    print(sprintf("generando  %.2f%% de ruido", ruidoPorcent * 100))
+    
+    if(ruidoPorcent == 0){
+      trainCopy <- trainDf
+    } else {
+      set.seed(unclass(Sys.time()))
+      idxRowRuido <- sample(nrow(trainDf), round(ruidoPorcent * nrow(trainDf)))
+      trainCopy <- trainDf
+      trainCopy[idxRowRuido, colIndex] <- valorClase
+    }
+    
+    resultado <- doTraining(trainCopy, testDf, "C", "confidenceFactor", seq(0.05, 0.5, 0.05))
+
+    dfPerformanceRuido <- rbind(dfPerformanceRuido, transform(resultado[[4]], porcentajeRuido = ruidoPorcent * 100))
+    dfSizeRuido <- rbind(dfSizeRuido, transform(resultado[[3]], porcentajeRuido = ruidoPorcent * 100))
+    
+  }
+  
+  dfSizeRuido$porcentajeRuido <- as.factor(dfSizeRuido$porcentajeRuido)
+  dfPerformanceRuido$porcentajeRuido <- as.factor(dfPerformanceRuido$porcentajeRuido)
+  
+  resultado <- list(dfSizeRuido, dfPerformanceRuido)
+  
+  plot1 <- ggplot(dfSizeRuido, aes(x = valorParam, y = leaves, color = porcentajeRuido)) +
+    geom_line() +
+    scale_color_hue("% Ruido") +
+    xlab("confidenceFactor") +
+    ylab("Hojas") +
+    theme_bw()
+  
+  plot2 <- ggplot(dfSizeRuido, aes(x = valorParam, y = nodes, color = porcentajeRuido)) +
+    geom_line() +
+    scale_color_hue("% Ruido") +
+    xlab("confidenceFactor") +
+    ylab("Nodos") +
+    theme_bw()
+  
+  plot3 <- ggplot(dfPerformanceRuido, aes(x = valorParam, y = accuracyTraining, color = porcentajeRuido)) +
+    geom_line() +
+    scale_color_hue("% Ruido") +
+    xlab("confidenceFactor") +
+    ylab("Accuracy (%)") +
+    theme_bw()
+  
+  resultado <- list.append(resultado, plot1, plot2, plot3)
+  
+  return(resultado)
+}
 
 # PUNTO 3 ########################################################################################## PUNT0 3
 #confidenceFactor -- The confidence factor used for pruning (smaller values incur more pruning).
@@ -156,14 +211,20 @@ savePlot(resultado[[2]], "Sobreajuste y poda (minNumObj) - Performance arbol", "
 
 
 # PUNTO 5 ########################################################################################## PUNT0 5
+faltantesModaTrain <- trainFaltantes("month_launched")
+savePlot(faltantesModaTrain[[3]], "Tratamiento de datos faltantes - Moda - Hojas arbol", "5 - moda - hojas.png")
+savePlot(faltantesModaTrain[[4]], "Tratamiento de datos faltantes - Moda - Nodos arbol", "5 - moda - nodos.png")
+savePlot(faltantesModaTrain[[5]], "Tratamiento de datos faltantes - Moda - Performance arbol", "5 - moda - performance.png")
+
+faltantesModaClaseTrain <- trainFaltantes("month_launched", FALSE)
+savePlot(faltantesModaClaseTrain[[3]], "Tratamiento de datos faltantes - Modaclase - Hojas arbol", "5 - modaclase - hojas.png")
+savePlot(faltantesModaClaseTrain[[4]], "Tratamiento de datos faltantes - Modaclase - Nodos arbol", "5 - modaclase - nodos.png")
+savePlot(faltantesModaClaseTrain[[5]], "Tratamiento de datos faltantes - Modaclase - Performance arbol", "5 - modaclase - performance.png")
 
 
-faltantesTrain <- trainFaltantes()
-savePlot(faltantesTrain[[3]], "Tratamiento de datos faltantes - Moda - Hojas arbol", "5 - moda - hojas.png")
-savePlot(faltantesTrain[[4]], "Tratamiento de datos faltantes - Moda - Nodos arbol", "5 - moda - nodo.png")
-savePlot(faltantesTrain[[5]], "Tratamiento de datos faltantes - Moda - Performance arbol", "5 - moda - performance.png")
+# PUNTO 6 ########################################################################################## PUNT0 6
+toleranciaRuidoTrain <- toleranciaRuido("funded", "no")
+savePlot(toleranciaRuidoTrain[[3]], "Tolerancia al ruido - Hojas arbol", "6 - hojas.png")
+savePlot(toleranciaRuidoTrain[[4]], "Tolerancia al ruido  - Nodos arbol", "6 - nodos.png")
+savePlot(toleranciaRuidoTrain[[5]], "Tolerancia al ruido - Performance arbol", "6 - performance.png")
 
-faltantesTrain <- trainFaltantes(FALSE)
-savePlot(faltantesTrain[[3]], "Tratamiento de datos faltantes - Modaclase - Hojas arbol", "5 - modaclase - hojas.png")
-savePlot(faltantesTrain[[4]], "Tratamiento de datos faltantes - Modaclase - Nodos arbol", "5 - modaclase - nodo.png")
-savePlot(faltantesTrain[[5]], "Tratamiento de datos faltantes - Modaclase - Performance arbol", "5 - modaclase - performance.png")
