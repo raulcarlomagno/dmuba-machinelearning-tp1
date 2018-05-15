f67b823d3502abb6627515b70c215e8d8259d648
Raul Carlomagno <rcarlomagno@gmail.com>
2018-05-15 11:18:14 -0300

    plots final, falta discretizacion supervisada

--- plots.R
+++ plots.R
@@ -6,7 +6,7 @@ testDf <- read.csv("ks-projects-processed-test.csv")
 # PUNTO 3 ########################################################################################## PUNT0 3
 #confidenceFactor -- The confidence factor used for pruning (smaller values incur more pruning).
 resultado <- doTraining(trainDf, testDf, "C", "confidenceFactor", seq(0.05, 0.5, 0.05))
-savePlot(resultado[[1]], "Sobreajuste y poda (CF) - Tamaño arbol", "3 - tamaño.png")
+savePlot(resultado[[1]], "Sobreajuste y poda (CF) - TamaÃ±o arbol", "3 - tamaÃ±o.png")
 savePlot(resultado[[2]], "Sobreajuste y poda (CF) - Performance arbol", "3 - perfomance.png")
 
 # PUNTO 4 ########################################################################################## PUNT0 4
@@ -15,7 +15,7 @@ minNumObjEnd <- round(0.10 * nrow(trainDf))
 minNumObjStep <- round(0.005 * nrow(trainDf))
 
 resultado <- doTraining(trainDf, testDf, "M", "minNumObj", seq(minNumObjStart, minNumObjEnd, minNumObjStep))
-savePlot(resultado[[1]], "Sobreajuste y poda (minNumObj) - Tamaño arbol", "4 - tamaño.png")
+savePlot(resultado[[1]], "Sobreajuste y poda (minNumObj) - TamaÃ±o arbol", "4 - tamaÃ±o.png")
 savePlot(resultado[[2]], "Sobreajuste y poda (minNumObj) - Performance arbol", "4 - perfomance.png")
 
 # PUNTO 5 ########################################################################################## PUNT0 5
@@ -39,11 +39,11 @@ savePlot(toleranciaRuidoTrain[[5]], "Tolerancia al ruido - Performance arbol", "
 
 # PUNTO 7 ########################################################################################## PUNT0 7
 discretizadoIgualAncho <- discretizar(1:20, c("usd_goal", "days_funding"))
-savePlot(discretizadoIgualAncho[[3]], "Discretización de atributos numéricos - Igual_ancho - Hojas arbol", "7 - igual_ancho - hojas.png", 125)
-savePlot(discretizadoIgualAncho[[4]], "Discretización de atributos numéricos - Igual_ancho - Nodos arbol", "7 - igual_ancho - nodos.png", 125)
-savePlot(discretizadoIgualAncho[[5]], "Discretización de atributos numéricos - Igual_ancho - Performance arbol", "7 - igual_ancho - performance testing.png", 125)
+savePlot(discretizadoIgualAncho[[3]], "DiscretizaciÃ³n de atributos numÃ©ricos - Igual_ancho - Hojas arbol", "7 - igual_ancho - hojas.png", 125)
+savePlot(discretizadoIgualAncho[[4]], "DiscretizaciÃ³n de atributos numÃ©ricos - Igual_ancho - Nodos arbol", "7 - igual_ancho - nodos.png", 125)
+savePlot(discretizadoIgualAncho[[5]], "DiscretizaciÃ³n de atributos numÃ©ricos - Igual_ancho - Performance arbol", "7 - igual_ancho - performance testing.png", 125)
 
 discretizadoIgualCantidad <- discretizar(1:20, c("usd_goal", "days_funding"), FALSE)
-savePlot(discretizadoIgualCantidad[[3]], "Discretización de atributos numéricos - Igual_cantidad - Hojas arbol", "7 - igual_ancho - hojas.png", 125)
-savePlot(discretizadoIgualCantidad[[4]], "Discretización de atributos numéricos - Igual_cantidad - Nodos arbol", "7 - igual_ancho - nodos.png", 125)
-savePlot(discretizadoIgualCantidad[[5]], "Discretización de atributos numéricos - Igual_cantidad - Performance arbol", "7 - igual_ancho - performance testing.png", 125)
+savePlot(discretizadoIgualCantidad[[3]], "DiscretizaciÃ³n de atributos numÃ©ricos - Igual_cantidad - Hojas arbol", "7 - igual_ancho - hojas.png", 125)
+savePlot(discretizadoIgualCantidad[[4]], "DiscretizaciÃ³n de atributos numÃ©ricos - Igual_cantidad - Nodos arbol", "7 - igual_ancho - nodos.png", 125)
+savePlot(discretizadoIgualCantidad[[5]], "DiscretizaciÃ³n de atributos numÃ©ricos - Igual_cantidad - Performance arbol", "7 - igual_ancho - performance testing.png", 125)
